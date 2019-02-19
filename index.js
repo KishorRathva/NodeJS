21a3636c46c7f7436a6bb23a209395a8156a416b
Kishor Rathva <kishor.rathva@rapidops.com>
2019-02-19 12:55:36 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const Logger = require('./logger');
+const fs = require('fs');
+const path = require('path');
 
 const logger = new Logger();
 
-logger.on('message',(data) => console.log('Called Listener:',data));
-logger.log('hello world');
-logger.log('hi');
-logger.log('hello');
+if (!fs.existsSync(path.join(__dirname,'/logs'))) {
+    fs.mkdir(path.join(__dirname,'/logs'),{},err => {
+		if (err) throw err ;
+		console.log('Folder created ..');
+	});
+};
+
+
+
+logger.on('message',(data) => {
+	fs.writeFile(path.join(__dirname,'/logs','logs.txt'),'', err => {
+		if (err) throw err ;
+	
+		// File append
+		fs.appendFile(path.join(__dirname,'/logs','logs.txt'),JSON.stringify(data)+'\r\n',err => {
+			if(err) throw err ;
+			console.log('File Written to....');
+		});
+		console.log("successfully loged..");
+	});
+});
+logger.log('hello world',new Date());
+logger.log('hello',new Date());
